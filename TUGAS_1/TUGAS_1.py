a569eff40bca85b490ca1460272a14c67dafeb4b
alimsatria <alimsatria2017@mail.ugm.ac.id>
2022-09-07 21:53:38 +0700

    add tugas_1

---
+++ TUGAS_1/TUGAS_1.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+
+image = cv.imread('lena.jpg')
+
+lebar = int(image.shape[0] * 80/100)
+panjang = int(image.shape[1] * 40/100)
+
+image = cv.resize(image,(lebar,panjang))
+
+cv.imshow('original image',image)
+
+# image.astype('float32')
+
+R = (image[:,:,2])
+G = (image[:,:,1])
+B = (image[:,:,0])
+
+RGB2G_formula = 0.2989 * R + 0.5870 *G + 0.1140 * B
+
+ret,biner_image = cv.threshold(RGB2G_formula,128,255,cv.THRESH_TOZERO_INV)
+ret,biner_image_ = cv.threshold(biner_image,90,90,cv.THRESH_TOZERO)
+
+cv.imshow("binary",biner_image_)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
